c0e8b671dd2308c56ebe6402788c25610a2ef29d
na-ja-f <najafnaju1983@gmail.com>
2024-07-18 10:13:03 +0530

    orgin change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(
     cors({
-        origin: '*',
+        origin: 'https://randompass-lemon.vercel.app',
         methods: "GET,HEAD,POST,DELETE",
         credentials: true,
     })
