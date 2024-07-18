551e003accfccd3e0d57c8cb23f2dea5c8505e8d
na-ja-f <najafnaju1983@gmail.com>
2024-07-18 10:00:26 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: "GET,HEAD,POST,DELETE",
         credentials: true,
     })
